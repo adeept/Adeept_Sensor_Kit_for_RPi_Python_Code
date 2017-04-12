4f0921cef88d3abbe49c8f26dd01446103501aaf
adeept <adeept@163.com>
2017-04-12 06:29:28 +0000

    modified the time of delay

--- 11_motor.py
+++ 11_motor.py
@@ -17,24 +17,24 @@ def setup():
 	motorStop()
 
 def motor(status, direction):
-	if status == 1:  # run
+	if status == 0: # stop
+		motorStop()
+	else:
 		if direction == 1:
 			GPIO.output(MotorPin_A, GPIO.HIGH)
 			GPIO.output(MotorPin_B, GPIO.LOW)
 		else:
 			GPIO.output(MotorPin_A, GPIO.LOW)
 			GPIO.output(MotorPin_B, GPIO.HIGH)
-	else:  # stop
-		motorStop()
 
 def loop():
 	while True:
 		motor(1, 1)
-		time.sleep(5000)
-		motor(0, 1)
-		time.sleep(5000)
+		time.sleep(5)
+		motor(0, 0) # stop
+		time.sleep(5)
 		motor(1, 0)
-		time.sleep(5000)
+		time.sleep(5)
 
 def destroy():
 	motorStop()
